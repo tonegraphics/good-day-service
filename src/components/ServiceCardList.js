892bd9eedadddfefa5b063c95cb08f42c04592b2
Taiga KIYOKAWA <taiga.k127@gmail.com>
2020-01-24 03:35:37 +0900

    separate 3 -> 4 services

--- src/components/ServiceCardList.js
+++ src/components/ServiceCardList.js
@@ -3,32 +3,30 @@ import Card from 'components/Card'
 
 const cardContents = [
   {
-    title: '地域密着型通所介護・生活介護',
+    title: '地域密着型通所介護',
     offerTime: '9:30 ~ 16:30',
-    target: [
-      {
-        type: '地域密着型通所介護',
-        detail: '介護認定で要介護1~5の認定を受けた方',
-      },
-      {
-        type: '生活介護',
-        detail: '18歳以上で障がい支援区分1~6の認定を受けた方',
-      },
-    ],
+    target: '介護認定で要介護1~5の認定を受けた方',
     imageSrc: 'service1.jpeg',
   },
   {
+    title: '生活介護',
+    offerTime: '9:30 ~ 16:30',
+    target:
+      '18歳以上で常時介護等の支援が必要な者で介護給付受給者証をお持ちの方',
+    imageSrc: 'service2.jpeg',
+  },
+  {
     title: '放課後等デイサービス',
-    offerTime: '平日(月~金)：15:30 ~ 17:30\n学休日(土・祝日)：9:30 ~ 16:30',
+    offerTime: '平日：15:00 ~ 17:30\n学休日：9:30 ~ 16:30',
     target:
       '障がい等お持ちの主に6歳から18歳までの\n学校に通っている子で、通所給付受給者証をお持ちの方',
-    imageSrc: 'service2.jpeg',
+    imageSrc: 'service3.jpeg',
   },
   {
     title: '児童発達支援',
     offerTime: '9:30 ~ 15:30',
     target: '障がい等お持ちの未就学の子で、\n通所給付受給者証をお持ちの方',
-    imageSrc: 'service3.jpeg',
+    imageSrc: 'service4.jpeg',
   },
 ]
 
