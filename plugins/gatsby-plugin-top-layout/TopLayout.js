1f09e7124c03b9a8824d1261588ed0272bfb8dbf
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-29 00:30:28 +0900

    developで問題ないことを確認

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
+import theme from '../../src/theme'
 
 export default function TopLayout(props) {
   return (
@@ -17,7 +18,7 @@ export default function TopLayout(props) {
           rel="stylesheet"
         />
       </Helmet>
-      <ThemeProvider>
+      <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         {props.children}
