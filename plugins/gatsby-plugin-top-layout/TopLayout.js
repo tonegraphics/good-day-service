b6dbdd0cf12086e22b8c57e5fa48f260944a7899
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-29 00:20:48 +0900

    Add plugin

---
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+export default function TopLayout(props) {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,800&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <ThemeProvider>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
+
+TopLayout.propTypes = {
+  children: PropTypes.node,
+}
