5a4e1ec3c1d871b1b02c3907a5f7ef7cc62152bc
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-29 02:03:10 +0900

    Fix H1

--- src/components/common/H1.js
+++ src/components/common/H1.js
@@ -1,29 +1,11 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
-import { colors } from 'libs/colors'
-import { fonts } from 'libs/fonts'
-import { useMediaQuery } from '@material-ui/core'
-
-const useStyles = makeStyles(theme => ({
-  title: props => ({
-    color: colors.brown,
-    fontSize: fonts(props.matches).headline,
-    borderBottom: `6px solid ${colors.red}`,
-    marginBottom: '16px',
-    display: 'inline-block',
-    '&:first-letter': {
-      fontSize: fonts(props.matches).firstLetter,
-    },
-  }),
-}))
+import Styles from '../../styles/components/common/H1.module.scss'
 
 export default props => {
-  const matches = useMediaQuery('(min-width: 1080px)')
-  const classes = useStyles({ matches })
   const { title } = props
   return (
     <div>
-      <h1 className={classes.title}>{title}</h1>
+      <h1 className={Styles.title}>{title}</h1>
     </div>
   )
 }
