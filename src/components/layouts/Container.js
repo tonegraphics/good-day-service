703e2e846fe0d14117c971ece5df599c267751f0
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-29 02:52:23 +0900

    Fix Container

--- src/components/layouts/Container.js
+++ src/components/layouts/Container.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/styles'
-import { useMediaQuery } from '@material-ui/core'
-import { colors } from 'libs/colors'
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    margin: '0 16px 40px',
-    padding: props => (props.matches ? '32px 32px 40px' : '16px 8px 40px'),
-    backgroundColor: colors.white,
-    borderRadius: 10,
-    maxWidth: 1048,
-  },
-}))
+import Styles from '../../styles/components/layouts/Container.module.scss'
 
 export default props => {
   const { body } = props
-  const matches = useMediaQuery('(min-width: 1080px)')
-  const classes = useStyles({ matches })
-  return <div className={classes.root}>{body}</div>
+  return <div className={Styles.root}>{body}</div>
 }
