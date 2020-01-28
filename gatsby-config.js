d5ca46294a4aac7c0678ac0ff198ff20408ec370
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-28 17:42:07 +0900

    Add plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
