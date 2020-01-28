b08d3b4c95ff810487daceb3f54dfc90a4a74323
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-29 03:08:16 +0900

    Fix FooterContainer

--- src/components/layouts/FooterContents.js
+++ src/components/layouts/FooterContents.js
@@ -1,73 +1,31 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { makeStyles } from '@material-ui/styles'
-import { useMediaQuery } from '@material-ui/core'
-import { colors } from 'libs/colors'
-import { fonts } from 'libs/fonts'
-
-const useStyles = makeStyles(theme => ({
-  contentsWrapper: props => ({
-    maxWidth: 1024,
-    margin: '0 auto',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: props.matches ? 'center' : 'flex-start',
-    alignItems: 'flex-start',
-    color: colors.yellow,
-    padding: '32px 16px',
-  }),
-  company: {
-    margin: '0 48px 32px 0',
-  },
-  companyName: props => ({
-    fontSize: fonts(props.matches).companyName,
-    marginBottom: 8,
-  }),
-  address: {
-    marginBottom: 8,
-  },
-  number: {
-    margin: 0,
-  },
-  link: props => ({
-    fontSize: fonts(props.matches).body,
-    color: colors.yellow,
-    display: 'block',
-    textDecoration: 'underline',
-    marginBottom: 20,
-    transition: '0.3s ease-in-out',
-    '&:hover': {
-      color: colors.red,
-    },
-  }),
-}))
+import Styles from '../../styles/components/layouts/FooterContainer.module.scss'
 
 export default () => {
-  const matches = useMediaQuery('(min-width: 1080px)')
-  const classes = useStyles({ matches })
   return (
-    <div className={classes.contentsWrapper}>
-      <div className={classes.company}>
-        <h3 className={classes.companyName}>GOOD ONE 株式会社</h3>
-        <p className={classes.address}>
+    <div className={Styles.contentsWrapper}>
+      <div className={Styles.company}>
+        <h3 className={Styles.companyName}>GOOD ONE 株式会社</h3>
+        <p className={Styles.address}>
           〒820-0001
           <br />
           福岡県飯塚市鯰田64番地1
         </p>
-        <p className={classes.number}>TEL: 0948-29-4112</p>
-        <p className={classes.number}>FAX: 0948-29-4144</p>
+        <p className={Styles.number}>TEL: 0948-29-4112</p>
+        <p className={Styles.number}>FAX: 0948-29-4144</p>
       </div>
       <div>
-        <Link to="/" className={classes.link}>
+        <Link to="/" className={Styles.link}>
           トップ
         </Link>
-        <Link to="/services" className={classes.link}>
+        <Link to="/services" className={Styles.link}>
           サービス
         </Link>
-        <Link to="/access" className={classes.link}>
+        <Link to="/access" className={Styles.link}>
           アクセス
         </Link>
-        <Link to="/company" className={classes.link}>
+        <Link to="/company" className={Styles.link}>
           会社概要
         </Link>
       </div>
